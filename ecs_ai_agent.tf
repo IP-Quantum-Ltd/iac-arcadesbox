3a8d6e882c51d923abed2fae388fa4271ea15fb5
rmartey <marteyrichmond001@gmail.com>
2026-05-13 16:40:49 +0000

    chore: Comment out AWS_REGION environment variable in ECS task definition

--- ecs_ai_agent.tf
+++ ecs_ai_agent.tf
@@ -112,7 +112,7 @@ resource "aws_ecs_task_definition" "ai_agent" {
         { name = "LANGSMITH_API_KEY", valueFrom = "${aws_secretsmanager_secret.ai_agent_secrets.arn}:LANGSMITH_API_KEY::" },
         { name = "LANGSMITH_PROJECT", valueFrom = "${aws_secretsmanager_secret.ai_agent_secrets.arn}:LANGSMITH_PROJECT::" },
         { name = "LANGSMITH_ENDPOINT", valueFrom = "${aws_secretsmanager_secret.ai_agent_secrets.arn}:LANGSMITH_ENDPOINT::" },
-        { name = "AWS_REGION", valueFrom = "${aws_secretsmanager_secret.ai_agent_secrets.arn}:AWS_REGION::" },
+        # { name = "AWS_REGION", valueFrom = "${aws_secretsmanager_secret.ai_agent_secrets.arn}:AWS_REGION::" },
         { name = "AWS_ACCESS_KEY_ID", valueFrom = "${aws_secretsmanager_secret.ai_agent_secrets.arn}:AWS_ACCESS_KEY_ID::" },
         { name = "AWS_SECRET_ACCESS_KEY", valueFrom = "${aws_secretsmanager_secret.ai_agent_secrets.arn}:AWS_SECRET_ACCESS_KEY::" },
         { name = "AWS_S3_BUCKET", valueFrom = "${aws_secretsmanager_secret.ai_agent_secrets.arn}:AWS_S3_BUCKET::" },
